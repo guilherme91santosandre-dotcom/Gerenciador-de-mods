18698574047b69fe7fcb8127d12943ec0a03ce42
Guilherme91 <guilherme91santosandre@gmail.com>
2026-02-09 10:08:00 +0000

    Atualização rápida

--- sys/1.7.60_22695/bauiv1/_hooks.py
+++ sys/1.7.60_22695/bauiv1/_hooks.py
@@ -1,8 +1,6 @@
 # Released under the MIT License. See LICENSE for details.
 #
-"""Snippets of code for use by the c++ layer."""
-# (most of these are self-explanatory)
-# pylint: disable=missing-function-docstring
+"""Safe hooks for BombSquad mods - disables broken buttons."""
 from __future__ import annotations
 
 import logging
@@ -12,8 +10,6 @@ from typing import TYPE_CHECKING
 import _bauiv1
 
 if TYPE_CHECKING:
-    from typing import Sequence
-
     import babase
     import bauiv1
 
@@ -22,130 +18,108 @@ def empty_call() -> None:
     pass
 
 
-def _root_ui_button_press(
-    rootuitype: bauiv1.UIV1AppSubsystem.RootUIElement,
-) -> None:
+def _root_ui_button_press(rootuitype: "bauiv1.UIV1AppSubsystem.RootUIElement") -> None:
+    """Call a UI button if it exists, else ignore."""
     import babase
 
-    ui = babase.app.ui_v1
+    ui = getattr(babase.app, "ui_v1", None)
+    if ui is None:
+        return
     call = ui.root_ui_calls.get(rootuitype)
     if call is not None:
         call()
 
 
+# --------------------------
+# Buttons that are safe to use
+# --------------------------
 def root_ui_account_button_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.ACCOUNT_BUTTON)
+    empty_call()
 
 
 def root_ui_inbox_button_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.INBOX_BUTTON)
+    empty_call()
 
 
 def root_ui_settings_button_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.SETTINGS_BUTTON)
+    empty_call()
 
 
 def root_ui_achievements_button_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.ACHIEVEMENTS_BUTTON)
+    empty_call()
 
 
+# --------------------------
+# Disable buttons that crash (store, tickets, etc)
+# --------------------------
 def root_ui_store_button_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.STORE_BUTTON)
-
-
-def root_ui_chest_slot_0_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.CHEST_SLOT_0)
-
-
-def root_ui_chest_slot_1_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.CHEST_SLOT_1)
-
-
-def root_ui_chest_slot_2_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.CHEST_SLOT_2)
-
-
-def root_ui_chest_slot_3_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.CHEST_SLOT_3)
-
-
-def root_ui_inventory_button_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.INVENTORY_BUTTON)
+    # Disabled to prevent crash
+    empty_call()
 
 
 def root_ui_ticket_icon_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.TICKETS_METER)
+    empty_call()
 
 
 def root_ui_get_tokens_button_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.GET_TOKENS_BUTTON)
+    empty_call()
 
 
 def root_ui_tokens_meter_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.TOKENS_METER)
+    empty_call()
 
 
 def root_ui_trophy_meter_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.TROPHY_METER)
+    empty_call()
 
 
 def root_ui_level_icon_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.LEVEL_METER)
+    empty_call()
 
 
 def root_ui_menu_button_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.MENU_BUTTON)
+    empty_call()
 
 
 def root_ui_back_button_press() -> None:
-    # Native layer handles this directly. (technically we could wire
-    # this up to not even come through Python).
-    _bauiv1.root_ui_back_press()
+    try:
+        _bauiv1.root_ui_back_press()
+    except Exception:
+        pass
 
 
 def root_ui_squad_button_press() -> None:
-    from bauiv1._appsubsystem import UIV1AppSubsystem
-
-    _root_ui_button_press(UIV1AppSubsystem.RootUIElement.SQUAD_BUTTON)
+    empty_call()
 
 
-def quit_window(quit_type: babase.QuitType) -> None:
+def root_ui_inventory_button_press() -> None:
+    empty_call()
+
+
+def root_ui_chest_slot_0_press() -> None:
+    empty_call()
+
+
+def root_ui_chest_slot_1_press() -> None:
+    empty_call()
+
+
+def root_ui_chest_slot_2_press() -> None:
+    empty_call()
+
+
+def root_ui_chest_slot_3_press() -> None:
+    empty_call()
+
+
+# --------------------------
+# Utility functions
+# --------------------------
+def quit_window(quit_type: "babase.QuitType") -> None:
     from babase import app
 
-    if app.classic is None:
-        logging.exception('Classic not present.')
+    if getattr(app, "classic", None) is None:
+        logging.exception("Classic not present.")
         return
 
     app.classic.quit_window(quit_type)
@@ -154,8 +128,8 @@ def quit_window(quit_type: babase.QuitType) -> None:
 def show_url_window(address: str) -> None:
     from babase import app
 
-    if app.classic is None:
-        logging.exception('Classic not present.')
+    if getattr(app, "classic", None) is None:
+        logging.exception("Classic not present.")
         return
 
     app.classic.show_url_window(address)
@@ -167,11 +141,9 @@ def double_transition_out_warning() -> None:
     caller_filename = caller_frame.filename
     caller_line_number = caller_frame.lineno
     logging.warning(
-        'ContainerWidget was set to transition out twice;'
-        ' this often implies buggy code (%s line %s).\n'
-        ' Generally you should check the value of'
-        ' _root_widget.transitioning_out and perform no actions if that'
-        ' is True.',
+        "ContainerWidget was set to transition out twice;"
+        " this often implies buggy code (%s line %s).\n"
+        "Generally you should check _root_widget.transitioning_out and do nothing if True.",
         caller_filename,
         caller_line_number,
     )
